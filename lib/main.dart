1f7119384d4f5745ec4741a43f70789356ee4b85
Rajul Sahu <74370799+RajulSahu@users.noreply.github.com>
2022-05-12 19:55:08 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  late List data;
+
+  Future<String> getData() async {
+    var url = Uri.https(
+      "jsonplaceholder.typicode.com",
+      "/users",
+    );
+    http.Response response = await http.get(url);
+    setState(() {
+      data = jsonDecode(response.body);
+    });
+    return "Done Fetching!";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text("API Integration")),
+      ),
+      body: ListView.builder(
+        itemCount: data == null ? 0 : data.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 10.0,
+              horizontal: 6.0,
+            ),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        data[index]['name'],
+                        style: const TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Text(
+                        data[index]['email'],
+                        style: const TextStyle(
+                          fontSize: 15.0,
+                          color: Colors.blueGrey,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    data[index]['username'],
+                    style: const TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                ]),
+          );
+        },
+      ),
+    );
+  }
+}
