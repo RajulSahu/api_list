e5cf0aa9fe34c0e1e79468af3b06b936c31ddb7d
Rajul Sahu <74370799+RajulSahu@users.noreply.github.com>
2022-05-14 21:50:26 +0530

    Updated the code by adding FutureBuilder to avoid late Initialization error.

--- lib/main.dart
+++ lib/main.dart
@@ -49,48 +49,67 @@ class HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: const Center(child: Text("API Integration")),
       ),
-      body: ListView.builder(
-        itemCount: data == null ? 0 : data.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            padding: const EdgeInsets.symmetric(
-              vertical: 10.0,
-              horizontal: 6.0,
-            ),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        data[index]['name'],
-                        style: const TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                      Text(
-                        data[index]['email'],
-                        style: const TextStyle(
-                          fontSize: 15.0,
-                          color: Colors.blueGrey,
-                          fontWeight: FontWeight.w300,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Text(
-                    data[index]['username'],
-                    style: const TextStyle(
-                      fontSize: 15.0,
-                      fontWeight: FontWeight.w300,
-                    ),
-                  ),
-                ]),
-          );
+      body: FutureBuilder(
+        future: getData(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            return ListItems(data: data);
+          }
         },
       ),
+    );
+  }
+}
+
+class ListItems extends StatelessWidget {
+  ListItems({required this.data});
+
+  final List data;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: data == null ? 0 : data.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10.0,
+            horizontal: 6.0,
+          ),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  data[index]['name'],
+                  style: const TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Text(
+                  data[index]['email'],
+                  style: const TextStyle(
+                    fontSize: 15.0,
+                    color: Colors.blueGrey,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              data[index]['username'],
+              style: const TextStyle(
+                fontSize: 15.0,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+          ]),
+        );
+      },
     );
   }
 }
